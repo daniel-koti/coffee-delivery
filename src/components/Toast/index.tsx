501b9edcb72a76be02eeb553c2e8a47f1861c2af
daniel-moniz <dkmoniz@cetim.ms>
2023-01-23 13:37:18 +0100

    feat: SEO

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,5 +1,5 @@
 import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import 'react-toastify/dist/ReactToastify.min.css'
 
 export function Toast() {
   return (
